42c7eb68bd7f976f63fc117e7c091dd1d8975171
Zookie <iushulgin@gmail.com>
2022-06-15 19:40:45 +0300

    .env

--- app.js
+++ app.js
@@ -5,6 +5,7 @@ const morgan = require('morgan');
 const path = require('path');
 const session = require('express-session');
 const FileStore = require('session-file-store')(session);
+const { checkSession } = require('./middleWares/middleWare');
 
 const homeRouter = require('./routes/homeRouter');
 const userRouter = require('./routes/userRouter');
@@ -23,6 +24,21 @@ app.use(morgan('dev'));
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 app.use(express.static(path.join(process.env.PWD, 'public')));
+
+app.use(session({
+  name: 'sID',
+  store: new FileStore({}),
+  secret: process.env.SESSION,
+  resave: true,
+  cookie: {
+    secure: false,
+    httpOnly: true,
+    maxAge: 1000 * 60 * 60 * 24,
+  },
+  saveUninitialized: false,
+}));
+
+app.use(checkSession);
 
 app.use('/', homeRouter);
 app.use('/user', userRouter);
--- db/database.js
+++ db/database.js
@@ -1,10 +1,12 @@
+require('dotenv').config();
+
 module.exports = {
 
   "development": {
-    "username": "admin",
-    "password": "123",
-    "database": "socker_site",
-    "host": "127.0.0.1",
+    "username": process.env.DB_USER,
+    "password": process.env.DB_PASS,
+    "database": process.env.DB_NAME,
+    "host": process.env.DB_URL,
     "dialect": "postgresql"
   },
   "test": {
