05a2f4b5bbc93cbc4db8f3da448f059ecd7c3ea3
Zookie <iushulgin@gmail.com>
2022-06-16 11:32:35 +0300

    Сиды поправил

--- db/seeders/20220615133222-all_seeds.js
+++ db/seeders/20220615133222-all_seeds.js
@@ -1,7 +1,5 @@
-'use strict';
-
 module.exports = {
-  async up (queryInterface, Sequelize) {
+  async up(queryInterface, Sequelize) {
     const users = [
       {
         name: 'Marina Noname',
@@ -75,7 +73,7 @@ module.exports = {
         pattern_url: 'pattern3.jpg',
         createdAt: new Date(),
         updatedAt: new Date(),
-      },``
+      },
       {
         pattern_name: 'Пёсики',
         pattern_url: 'pattern4.jpg',
@@ -131,7 +129,7 @@ module.exports = {
     await queryInterface.bulkInsert('Favorites', favorites);
   },
 
-  async down (queryInterface, Sequelize) {
+  async down(queryInterface, Sequelize) {
     await queryInterface.bulkDelete('Users', null, {});
     await queryInterface.bulkDelete('Socks', null, {});
     await queryInterface.bulkDelete('Patterns', null, {});
@@ -139,5 +137,5 @@ module.exports = {
     await queryInterface.bulkDelete('Combinations', null, {});
     await queryInterface.bulkDelete('Carts', null, {});
     await queryInterface.bulkDelete('Favorites', null, {});
-  }
+  },
 };
--- routes/generatorRouter.js
+++ routes/generatorRouter.js
@@ -1,6 +1,8 @@
 const router = require('express').Router();
+const { Pattern, Picture, Sock } = require('../db/models');
 
 router.get('/generator', async (req, res) => {
+  const patterns = await Pattern({})
   res.render('generator');
 });
 
