fd1835233fa53351b5213e46950d256889ad2e0f
Maria <zaytseva.m.v@yandex.ru>
2022-06-17 17:10:48 +0300

    some changes

--- public/js/changeQty.js
+++ public/js/changeQty.js
@@ -1,23 +1,13 @@
-const form = document.forms[0];
-const changeQtyMinus = document.querySelector('.btn btn-primary changeQtyMinus');
-const changeQtyPlus = document.querySelector('.btn btn-primary changeQtyPlus');
-// const qtyToDB = {qty}
-// const qty = document.querySelector('.btn btn-primary qty')
+const buttons = document.getElementsByClassName('cart-container')
 
-form.addEventListener('click', async (event) => {
-  // console.log(event.target.className);
-  if (event.target.className === "btn btn-primary changeQtyMinus" || event.target.className === "btn btn-primary changeQtyPlus") {
-    console.log('goodJOB');
-  //   const response = await fetch('/favorites', {
-  //     method: 'put',
-  //     headers: {
-  //       'Content-Type': 'application/json',
-  //     },
-  //     body: JSON.stringify(dataToDB),
-  //   });
-  //   if (response.ok) {
-  //     await response
-  //     window.location = "/favorites"
-  //   }
+document.addEventListener('click', async (event) => {
+  // console.log(event.target);
+  if (event.target.classList.contains("btn-primary")) {
+    const favButton = document.querySelector('.btn-primary')
+
+    console.log(favButton.closest('.product'));
+    // console.log(favButton.closest('#sock'))
+    // const combo_id = document.querySelector('div[name="productCart"]')
+    // console.log(combo_id);
   }
 })
