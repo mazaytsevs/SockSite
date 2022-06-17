0034695318698bf73ef34c4564b87e5273ebdfd1
MaratEsiev <marik744056@gmail.com>
2022-06-17 15:43:30 +0300

    deletion fixed

--- public/js/deleteFavor.js
+++ public/js/deleteFavor.js
@@ -1,24 +1,24 @@
 const del = document.querySelector('.favorites-container');
 del.addEventListener('click', async (event) => {
   event.preventDefault();
-  if (event.target.nodeName === '.delete-button') {
+  if (event.target.classList.contains('delete-button')) {
     const mar = event.target.closest('[data-id]');
     console.log(mar);
-  }
+    const li = event.target.closest('[data-id]');
+    console.log('li: ', li);
 
-  const li = event.target.closest('[data-id]');
-  console.log('li: ', li);
+    const { id } = li.dataset;
+    console.log('id: ', { id });
 
-  const { id } = li.dataset;
-  console.log('id: ', { id });
+    const response = await fetch(`/favorites/${id}`, {
+      method: 'DELETE',
+      // headers: {
+      //   'Content-Type': 'application/json;charset=utf-8',
+      // },
 
-  const response = await fetch(`/favorites/${id}`, {
-    method: 'DELETE',
-    // headers: {
-    //   'Content-Type': 'application/json;charset=utf-8',
-    // },
-  });
-  if (response.ok) {
-    li.remove();
+    });
+    if (response.ok) {
+      li.remove();
+    }
   }
 });
