1b81e7511b3e637c60600cd72059a7c990763fd4
Maria <zaytseva.m.v@yandex.ru>
2022-06-17 14:02:34 +0300

    test

--- public/js/changeQty.js
+++ public/js/changeQty.js
@@ -1,13 +1,13 @@
 const form = document.forms[0];
 const changeQtyMinus = document.querySelector('.btn btn-primary changeQtyMinus');
 const changeQtyPlus = document.querySelector('.btn btn-primary changeQtyPlus');
-const qty = document.querySelector('.btn btn-primary qty')
+// const qtyToDB = {qty}
+// const qty = document.querySelector('.btn btn-primary qty')
 
 form.addEventListener('click', async (event) => {
   // console.log(event.target.className);
-  console.log(qty);
-  // if (event.target.className === "btn btn-primary changeQtyMinus" || event.target.className === "btn btn-primary changeQtyPlus") {
-  //   console.log('goodJOB');
+  if (event.target.className === "btn btn-primary changeQtyMinus" || event.target.className === "btn btn-primary changeQtyPlus") {
+    console.log('goodJOB');
   //   const response = await fetch('/favorites', {
   //     method: 'put',
   //     headers: {
@@ -19,5 +19,5 @@ form.addEventListener('click', async (event) => {
   //     await response
   //     window.location = "/favorites"
   //   }
-  // }
+  }
 })
