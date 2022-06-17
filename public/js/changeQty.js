a971d6ba8f568adbfd651f3731fe7c1f5a976f3b
Maria <zaytseva.m.v@yandex.ru>
2022-06-17 12:42:24 +0300

    some work with DOM

---
+++ public/js/changeQty.js
@@ -0,0 +1,23 @@
+const form = document.forms[0];
+const changeQtyMinus = document.querySelector('.btn btn-primary changeQtyMinus');
+const changeQtyPlus = document.querySelector('.btn btn-primary changeQtyPlus');
+const qty = document.querySelector('.btn btn-primary qty')
+
+form.addEventListener('click', async (event) => {
+  // console.log(event.target.className);
+  console.log(qty);
+  // if (event.target.className === "btn btn-primary changeQtyMinus" || event.target.className === "btn btn-primary changeQtyPlus") {
+  //   console.log('goodJOB');
+  //   const response = await fetch('/favorites', {
+  //     method: 'put',
+  //     headers: {
+  //       'Content-Type': 'application/json',
+  //     },
+  //     body: JSON.stringify(dataToDB),
+  //   });
+  //   if (response.ok) {
+  //     await response
+  //     window.location = "/favorites"
+  //   }
+  // }
+})
